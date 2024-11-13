2c6a0abe1b9d64909e69b21e1ed7b4955fb43ea6
Vineeth Dasaraju <vineethd123@gmail.com>
2024-11-13 14:01:26 -0800

    Initial Commit

---
+++ src/main/java/com/vineeth/harness_demo/HarnessDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vineeth.harness_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarnessDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarnessDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vineeth/harness_demo/HarnessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineeth.harness_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
